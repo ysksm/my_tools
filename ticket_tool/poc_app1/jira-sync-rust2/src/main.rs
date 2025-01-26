3bbe012eecc349226e51bbc9bd8606732c167b9f
kasama <ka.sa.ma.123@gmail.com>
2025-01-27 00:29:53 +0900

    途中

--- ticket_tool/poc_app1/jira-sync-rust2/src/main.rs
+++ ticket_tool/poc_app1/jira-sync-rust2/src/main.rs
@@ -40,6 +40,15 @@ struct Response {
     values: Vec<serde_json::Value>,
 }
 
+#[derive(Debug, Deserialize)]
+struct Response2 {
+    // issues: Vec<serde_json::Value>,
+    // names: Vec<serde_json::Value>,
+    // schema: Vec<serde_json::Value>,
+    #[serde(rename = "nextPageToken")]
+    next_page_token: Option<String>,
+}
+
 const PROJECT_JSON_FILE_PATH: &str = "projects_search";
 const FIELDS_JSON_FILE_PATH: &str = "fields_search";
 
@@ -86,30 +95,6 @@ async fn request_get(url: &str, headers: HeaderMap) -> Result<String, Box<dyn Er
     Ok(body)
 }
 
-async fn request_post(url: &str, headers: HeaderMap, request_body: &HashMap<String, String> ) -> Result<String, Box<dyn Error>> {
-    println!("Requesting: {}", url);
-
-    let client = reqwest::Client::new();
-    let response = client
-        .post(url)
-        .headers(headers)
-        .json(request_body)
-        .send()
-        .await?;
-
-    println!("Status: {}", response.status());
-    
-    if !response.status().is_success() {
-        return Err(format!("Request failed with status: {}", response.status()).into());
-    }
-
-    let body = response.text().await?;
-
-    println!("Response: {}", body);
-
-    Ok(body)
-}
-
 async fn request_api(headers: HeaderMap, url: &str, dir_path: &str) {
 
     let mut is_last = false;
@@ -149,77 +134,14 @@ async fn request_api(headers: HeaderMap, url: &str, dir_path: &str) {
 
 }
 
-async fn init_db(config: &Config, headers: &HeaderMap) -> Result<(), Box<dyn Error>> {
+async fn init_db(config: &Config, headers: &HeaderMap, conn: Connection) -> Result<(), Box<dyn Error>> {
 
     // プロジェクト一覧取得
     let projects_search_url = format!("{}/rest/api/3/project/search?expand=description,projectKeys,lead,issueTypes,url,insight", config.api.jira_base_url);
     let projects_ourput_dir = format!("{}/{}", config.setting.output_dir, PROJECT_JSON_FILE_PATH);
     request_api(headers.clone(), &projects_search_url, &projects_ourput_dir).await;
 
-    // フィールド一覧取得
-    let filed_painated_url = format!("{}/rest/api/3/field/search?orderBy=name", config.api.jira_base_url);
-    let fields_output_dir = format!("{}/{}", config.setting.output_dir, FIELDS_JSON_FILE_PATH);
-    request_api(headers.clone(), &filed_painated_url, &fields_output_dir).await;
-
-    Ok(())
-}
-
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-
-    // 設定ファイルを読み込む
-    let config_data = fs::read_to_string("config.toml").await?;
-    let config: Config = toml::from_str(&config_data)?;
-
-    let db_path = "jira.db";
-
-
-    // ヘッダーを作成
-    let headers = create_headers(&config.api.jira_username, &config.api.jira_api_token).await?;
-
-    init_db(&config, &headers).await?;
-
-
-    // Issue検索
-    let jql = "project=todo";
-    let max_results = 50;
-    let fields = ["summary"];
-    let fiedlds_str = format!("[\"{}\"]", fields.join("\",\""));
-    println!("Fields: {}", fiedlds_str);
-    let expand = "changelog,names";
-    let issue_output_dir = format!("{}/issues", config.setting.output_dir);
-
-    let issue_url = format!("{}/rest/api/3/search/jql", config.api.jira_base_url);
-    let mut request_body = HashMap::new();
-    request_body.insert("jql".to_string(), jql.to_string());
-    request_body.insert("maxResults".to_string(), max_results.to_string());
-    // request_body.insert("fields".to_string(), fiedlds);
-    // request_body.insert("expand".to_string(), expand.to_string());
-
-    let response = request_post(&issue_url, headers.clone(), &request_body).await;
-    match response {
-        Ok(body) => {
-            if !std::path::Path::new(&issue_output_dir).exists() {
-                fs::create_dir_all(&issue_output_dir).await.unwrap();
-            }
-            let file_path = format!("{}/issues.json", issue_output_dir);
-            fs::write(&file_path, &body).await.unwrap();
-        },
-        Err(e) => {
-            println!("Error: {}", e);
-        }
-    }
-
-    // dbファイルが存在したら削除する
-    let db_path = format!("{}/{}", &config.setting.output_dir, db_path);
-    if std::path::Path::new(&db_path).exists() {
-        fs::remove_file(&db_path).await?;
-        println!("Removed existing db file");
-    }
-    let conn = Connection::open(db_path)?;
-
-    // プロジェクト一覧テーブル作成
+        // プロジェクト一覧テーブル作成
     let create_sql = format!("CREATE TABLE projects AS 
         SELECT 
             unnest(values)->>'id' as id,
@@ -238,24 +160,137 @@ async fn main() -> Result<(), Box<dyn Error>> {
         FROM read_json_auto('./{}/{}/*.json')", &config.setting.output_dir, PROJECT_JSON_FILE_PATH);
     conn.execute(&create_sql, params![])?;
 
-    // フィールド一覧テーブル作成
-    let create_sql = format!("CREATE TABLE fields AS 
-        SELECT 
-            unnest(values)->>'id' as id,
-            unnest(values)->>'name' as name,
-            unnest(values)->>'custom' as custom,
-            unnest(values)->>'orderable' as orderable,
-            unnest(values)->>'navigable' as navigable,
-            unnest(values)->>'searchable' as searchable,
-            unnest(values)->>'clauseNames' as clauseNames,
-            unnest(values)->>'schema' as schema,
-            unnest(values)->>'schema'->>'type' as schema_type,
-            unnest(values)->>'schema'->>'system' as schema_system,
-            unnest(values)->>'schema'->>'items' as schema_items,
-            unnest(values)->>'schema'->>'custom' as schema_custom,
-            unnest(values)->>'schema'->>'customId' as schema_customId,
-        FROM read_json_auto('./{}/{}/*.json')", &config.setting.output_dir, FIELDS_JSON_FILE_PATH);
-    conn.execute(&create_sql, params![])?;
+    // フィールド一覧取得
+    let field_paginated_url = format!("{}/rest/api/3/field/search?orderBy=name", config.api.jira_base_url);
+    let fields_output_dir = format!("{}/{}", config.setting.output_dir, FIELDS_JSON_FILE_PATH);
+    request_api(headers.clone(), &field_paginated_url, &fields_output_dir).await;
+
+    Ok(())
+}
+
+async fn request_post(url: &str, headers: HeaderMap, request_body: &HashMap<String, serde_json::Value> ) -> Result<String, Box<dyn Error>> {
+    println!("Requesting: {}", url);
+
+    let client = reqwest::Client::new();
+    let response = client
+        .post(url)
+        .headers(headers)
+        .json(request_body)
+        .send()
+        .await?;
+
+    println!("Status: {}", response.status());
+    
+    if !response.status().is_success() {
+        return Err(format!("Request failed with status: {}", response.status()).into());
+    }
+
+    let body = response.text().await?;
+
+    println!("Response: {}", body);
+
+    Ok(body)
+}
+
+async fn create_issue_request_body(jql: &str, fields: Vec<String>, next_page_token: Option<String>) -> HashMap<String, serde_json::Value> {
+
+   // Issue検索
+   let expand = "renderedFields,names,schema,transitions,operations,editmeta,changelog,versionedRepresentations";
+//    let fields = vec!["summary", "description", "status"];  // 必要なフィールドを追加
+   let max_results = 5;
+
+    let mut request_body = HashMap::new();
+    request_body.insert("expand".to_string(), serde_json::Value::String(expand.to_string()));
+    request_body.insert("fields".to_string(), serde_json::Value::Array(fields.into_iter().map(|s| serde_json::Value::String(s.to_string())).collect()));
+    request_body.insert("jql".to_string(), serde_json::Value::String(jql.to_string()));
+    request_body.insert("maxResults".to_string(), serde_json::Value::Number(max_results.into()));
+    if next_page_token.is_some() {
+        request_body.insert("nextPageToken".to_string(), serde_json::Value::String(next_page_token.unwrap().to_string()));
+    }
+    request_body
+}
+
+async fn sync_issues(config: &Config,headers: HeaderMap, fields: Vec<String>) -> Result<(), Box<dyn Error>> {
+
+    let url = format!("{}/rest/api/3/search/jql", config.api.jira_base_url);
+    let dir_path = format!("{}/issues", config.setting.output_dir);
+    if !std::path::Path::new(&dir_path).exists() {
+        fs::create_dir_all(&dir_path).await.unwrap();
+    }
+
+    let mut is_last = false;
+    let mut page_count = 0;
+    let mut next_page_token = None;
+    let jql = "project=todo order by ";
+
+    while !is_last {
+        let request_body = create_issue_request_body(&jql, fields.clone(), next_page_token.clone()).await;
+        let response = request_post(&url, headers.clone(), &request_body).await;
+        match response {
+            Ok(body) => {
+                let file_path = format!("{}/{}.json", dir_path, page_count);
+                fs::write(&file_path, &body).await.unwrap();
+                let response: Response2 = serde_json::from_str(&body).unwrap();
+
+                if let Some(next_page_token_text) = response.next_page_token {
+                    next_page_token = Some(next_page_token_text);
+                } else {
+                    next_page_token = None;
+                    is_last = true;
+                }
+            },
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+        page_count = page_count + 1;
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+
+    // 設定ファイルを読み込む
+    let config_data = fs::read_to_string("config.toml").await?;
+    let config: Config = toml::from_str(&config_data)?;
+
+    let db_path = "jira.db";
+    // dbファイルが存在したら削除する
+    let db_path = format!("{}/{}", &config.setting.output_dir, db_path);
+    if std::path::Path::new(&db_path).exists() {
+        fs::remove_file(&db_path).await?;
+        println!("Removed existing db file");
+    }
+    let conn = Connection::open(db_path)?;
+    // ヘッダーを作成
+    let headers = create_headers(&config.api.jira_username, &config.api.jira_api_token).await?;
+    init_db(&config, &headers, conn).await?;
+
+    // sync_issues(&config, headers).await?;
+
+
+
+
+
+    // // フィールド一覧テーブル作成
+    // let create_sql = format!("CREATE TABLE fields AS 
+    //     SELECT 
+    //         unnest(values)->>'id' as id,
+    //         unnest(values)->>'name' as name,
+    //         unnest(values)->>'custom' as custom,
+    //         unnest(values)->>'orderable' as orderable,
+    //         unnest(values)->>'navigable' as navigable,
+    //         unnest(values)->>'searchable' as searchable,
+    //         unnest(values)->>'clauseNames' as clauseNames,
+    //         unnest(values)->>'schema' as schema,
+    //         unnest(values)->>'schema'->>'type' as schema_type,
+    //         unnest(values)->>'schema'->>'system' as schema_system,
+    //         unnest(values)->>'schema'->>'items' as schema_items,
+    //         unnest(values)->>'schema'->>'custom' as schema_custom,
+    //         unnest(values)->>'schema'->>'customId' as schema_customId,
+    //     FROM read_json_auto('./{}/{}/*.json')", &config.setting.output_dir, FIELDS_JSON_FILE_PATH);
+    // conn.execute(&create_sql, params![])?;
 
     conn.close().unwrap();
     println!("Done");
